f124fefe16b31ea6cdb6ee63798310a6b69adb79
conmccoid <conmccoid@gmail.com>
2025-06-17 13:10:02 -0400

    strong scaling in blocks example

--- MATLAB/EX_Blocks.m
+++ MATLAB/EX_Blocks.m
@@ -81,44 +81,49 @@ hold off
 close all
 clear all
 
-ny=11;
-h=0.1;
-
-list_blocks=2:5; list_blocks=2.^list_blocks;
+list_blocks=2:5; block_size=3*4;
 conv_rate = zeros(length(list_blocks),1); k=1;
 err_master = cell(length(list_blocks),1);
 
-nx=max(list_blocks)*(ny-1) + 1;
+ny=max(list_blocks)*block_size + 1;
+nx=ny;
+h=1/(ny-1);
+
 [A,ind] = MAT_Laplace_rectangle(nx,ny,h);
 f = -ones(nx*ny,1); f(ind)=0;
 u_exact = A \ f;
 
 for n_blocks=list_blocks
 
+    block_size=(ny-1)/n_blocks;
+
 %     figure(1)
 %     surf(reshape(u_exact,ny,nx))
 
-    % ind_strips subroutine - nb: redo for strong scaling
-    ind_blocks=cell(n_blocks+1,1);
-    N_strip=ny*(nx-1-n_blocks)/n_blocks;
-    ind_strip=1:N_strip;
-    ind_i=ny+N_strip;
-    ind_blocks{1}=1:ind_i;
-    indtr=ind_i+(1:ny);
-    ind_i=ind_i+ny;
-    for i=2:n_blocks-1
-        ind_blocks{i}=ind_i+ind_strip;
-        ind_i=ind_i+N_strip;
-        indtr=[indtr,ind_i+(1:ny)];
-        ind_i=ind_i+ny;
-    end
-    ind_end=ind_i+ind_strip;
-    ind_i=ind_i+N_strip;
-    ind_end=[ind_end,ind_i+(1:ny)];
-    ind_blocks{n_blocks}=ind_end;
-    ind_blocks{end}=indtr;
+    % ind_blocks subroutine
+    x = linspace(0,1,nx);
+    xx= kron(x,ones(1,nx));
+    yy= kron(ones(1,nx),x);
+    ind_master=1:nx*ny;
 
-    [u,err] = ALGO_trAOSM(A,f,ind_blocks,rand(length(indtr),1),nx,ny);
+    ind_blocks=cell(n_blocks+1,1);
+    ind_tr=[];
+    count=1;
+    x(1)=-1; x(end)=2;
+    for i=1:n_blocks
+        ind_x = xx < x(i*block_size+1) & xx > x((i-1)*block_size+1);
+        for j=1:n_blocks
+            ind_y = yy < x(j*block_size+1) & yy > x((j-1)*block_size+1);
+            block_int=ind_master(ind_x & ind_y);
+            crosspoint=ind_master(xx==x(i*block_size+1) & yy==x(j*block_size+1));
+            ind_blocks{count}=[block_int, crosspoint]; count=count+1;
+            ind_tr=[ind_tr,ind_master(xx==x(i*block_size+1) & ind_y)];
+            ind_tr=[ind_tr,ind_master(ind_x & yy==x(j*block_size+1))];
+        end
+    end
+    ind_blocks{count}=ind_tr;
+
+    [u,err] = ALGO_trAOSM(A,f,ind_blocks,rand(length(ind_tr),1),nx,ny);
     P = polyfit(1:length(err),log(err),1);
     conv_rate(k)=exp(P(1));
     err_master{k}=err;
@@ -133,8 +138,8 @@ for n_blocks=list_blocks
 end
 
 figure
-semilogx(list_blocks,abs(conv_rate),'bo--')
-xlabel('Number of strips')
+plot(list_blocks.^2,abs(conv_rate),'bo--')
+xlabel('Number of blocks')
 ylabel('Convergence rate')
 
 figure
